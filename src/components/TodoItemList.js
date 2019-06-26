563da0c9d57607def6b3f7fc86c249d59cfac325
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-06-26 15:27:45 +0900

    TodoItem Component

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
-    return <div />;
+    return (
+      <div>
+        <TodoItem text="안녕" />
+        <TodoItem text="React js" />
+        <TodoItem text="hello" />
+      </div>
+    );
   }
 }
 
